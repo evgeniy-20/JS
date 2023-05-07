28b4702f4c1cc445bd81ed14e4c167b00a790db3
evgeniy20 <evgeniy.prokopiv@gmail.com>
2023-05-07 19:47:56 +0300

    task-2

--- JavaScript task by levels/level-1/app.js
+++ JavaScript task by levels/level-1/app.js
@@ -52,3 +52,44 @@ if (word[word.length - 1] === "ь") {
   lastLetter = word[word.length - 1]; 
 }
 console.log(lastLetter);
+
+//Дано число. Виведіть у консоль першу цифру цього числа.
+
+let num1 = 564;
+
+if(num1 >= 10) {
+  num1 = Math.floor(num1 / 10);
+}
+
+console.log(num1);
+
+//Дано число. Виведіть у консоль останню цифру цього числа.
+
+const num2 = 54678
+const lastDigit = num2 % 10;
+console.log(lastDigit)
+
+//Дано число. Виведіть у консоль суму першої та останньої цифри цього числа.
+
+const num = 76543;
+const lastDigit1 = num % 10;
+const firstDigit = Math.floor(num / Math.pow(10, (num.toString().length - 1)));
+const sum = lastDigit1 + firstDigit;
+console.log(sum);
+
+//Дано число. Виведіть кількість цифр у цьому числі.
+
+const num3 = 1234567890;
+const newNum= num3.toString().length;
+console.log(newNum);
+
+//Дано два числа. Перевірте, чи перші цифри цих чисел збігаються.
+
+const num4 = 12345;
+const num5 = 16789;
+
+if (num4.toString()[0] === num5.toString()[0]) {
+  console.log("True");
+} else {
+  console.log("False");
+}
