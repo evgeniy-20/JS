6f3dafaef49e767c512e83b48c3fddf88745a270
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-04 11:44:43 +0200

    practice

---
+++ Цикл/Практика/19.js
@@ -0,0 +1,17 @@
+// Дано наступний об'єкт із працівниками та їх зарплатами:  Збільште зарплату кожного працівника на 10%.
+
+let obj = {
+    employee1: 100,
+    employee2: 200,
+    employee3: 300,
+    employee4: 400,
+    employee5: 500,
+    employee6: 600,
+    employee7: 700,
+};
+
+for (let key in obj) {
+    obj[key] *= 1.1;
+}
+
+console.log(obj);
