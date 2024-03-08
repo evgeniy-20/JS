df480a117725edbfb5c8a993b51256b1b6acbb37
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-08 20:52:33 +0200

    Глобальні змінні у функціях у JavaScript

---
+++ Змінні функції/Глобальні змінні у функціях у JavaScript/2.js
@@ -0,0 +1,9 @@
+// Визначте, не запускаючи код, що виведеться в консоль:
+let num = 1;
+
+function func() {
+    console.log(num);
+}
+
+num = 2;
+func();
---
+++ Змінні функції/Глобальні змінні у функціях у JavaScript/3.js
@@ -0,0 +1,12 @@
+// Визначте, не запускаючи код, що виведеться в консоль:
+function func() {
+    console.log(num);
+}
+
+let num;
+
+num = 1;
+func();
+
+num = 2;
+func();
