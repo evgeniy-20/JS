ebcc3cf397a5e5417a407b1fee11b2f99fd646f9
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-09 21:29:11 +0200

    game Gallows

---
+++ game Gallows/app.js
@@ -0,0 +1,11 @@
+// Кодуємо гру
+// Обираємо випадкове слово
+
+let words = [
+    'javaScript',
+    'monkey',
+    'amazing',
+    'pancake'
+]
+
+let word = words[Math.floor(Math.random() * words.length)];
