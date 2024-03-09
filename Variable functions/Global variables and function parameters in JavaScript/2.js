c940867fae261c484b94a1117b551ba3fca985f0
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-09 21:49:59 +0200

    Global variables and function parameters in JavaScript

---
+++ Variable functions/Global variables and function parameters in JavaScript/2.js
@@ -0,0 +1,8 @@
+// Визначте, не запускаючи код, який виведеться в консоль:
+
+function func(localNum) {
+    console.log(localNum);
+}
+
+let num = 1;
+func(num);
---
+++ Variable functions/Global variables and function parameters in JavaScript/4.js
@@ -0,0 +1,10 @@
+// Визначте, не запускаючи код, який виведеться в консоль:
+
+let num = 1;
+
+function func(localNum) {
+    console.log(localNum);
+}
+
+num = 2;
+func(num);
