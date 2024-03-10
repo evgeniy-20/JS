70d63a3cb0c59f97286b51a75cdf13b758b0ff27
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-10 22:14:54 +0200

    JavaScript object parameters

--- Variable functions/JavaScript object parameters/6.js
+++ Variable functions/JavaScript object parameters/6.js
@@ -5,6 +5,23 @@ function func(arr) {
     newArr[0] = '!';
 }
 
-let arr = [1, 2, 3];
-func(arr);
-console.log(arr);
+// let arr = [1, 2, 3];
+// func(arr);
+// console.log(arr);
+
+let arr = [1, 2, 3, 4, 5, 3];
+for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] % 3 === 0) {
+        console.log(arr[i]);
+    }
+    break
+}
+
+let wallWidth = 100;
+let cabinetWidth = 30;
+
+function numberOfCabinet(wallWidth, cabinetWidth) {
+    return Math.floor(wallWidth / cabinetWidth)
+}
+
+console.log(numberOfCabinet(wallWidth, cabinetWidth));
