2116bf421a0f9b74940dd5ec2646fe4b7a49a014
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-14 22:17:57 +0200

    ested functions

--- Nested functions/Named parameter functions in JavaScript/1.js
+++ Nested functions/Named parameter functions in JavaScript/1.js
@@ -1,1 +1,18 @@
-// 
+//  Зробіть функцію test, параметрами приймаючої 3 функції і суму результатів переданих функцій.
+
+let get1 = function () {
+    return 1;
+}
+let get2 = function () {
+    return 2;
+}
+
+let get3 = function () {
+    return 3;
+}
+
+test(get1, get2, get3);
+
+function test(func1, func2, func3) {
+    console.log(func1() + func2() + func3());
+}
