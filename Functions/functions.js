55393d24e85be893da820b2e28637374ad77c8d9
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-24 22:14:42 +0200

    function

---
+++ Functions/functions.js
@@ -0,0 +1,47 @@
+// Створюємо просту функцію
+
+let ourFirstFunction = function () {
+    console.log('Hello world!');
+};
+
+
+// Викликаємо функцію
+
+ourFirstFunction();
+
+// передаємо функціям аргументи
+
+let sayHelloTo = function (name) {
+    console.log('Hello ' + name + '!');
+};
+
+sayHelloTo('Yevhen');
+
+// Друкуємо котячі мордочки
+
+let drawCats = function (howManyTimes) {
+    for (let i = 0; i < howManyTimes; i++) {
+        console.log(i + '=^.^=');
+    }
+};
+
+drawCats(10);
+
+// Передаємо функції декілька аргументів
+
+let printMultipleTimes = function (howManyTimes, whatToDraw) {
+    for (let i = 0; i < howManyTimes; i++) {
+        console.log(i + ' ' + whatToDraw);
+    }
+};
+printMultipleTimes(4, '^.^');
+
+// Повертаємо значення з функції
+
+let double = function (number) {
+    return number * 2;
+};
+
+console.log(double(3));
+
+// Функція для оброблення випадкового слова
