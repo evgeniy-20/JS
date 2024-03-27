4f73d6c31156854a4f6beb486c09b7fc5ef606fc
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-27 06:45:30 +0200

    functions

--- Functions/functions.js
+++ Functions/functions.js
@@ -61,4 +61,28 @@ function multiplay(num1, num2) {
     return num1 * num2;
 }
 
-console.log(multiplay(36325, 9824) + 777);
+let result = add(multiplay(36325, 9824), 777);
+
+console.log(result);
+
+// чи однакові функції
+
+function areArraysSame(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(areArraysSame([1, 2, 3], [4, 5, 6])); // false
+console.log(areArraysSame([1, 2, 3], [1, 2, 3])); // true
+console.log(areArraysSame([1, 2, 3], [1, 2, 3, 4])); // false
+
+// Створюємо гру шибениця, використовуючи функції
