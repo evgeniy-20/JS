8a8f4a04c6e4e12e515b4bcc1f1bfbe2a3938b13
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-03-28 06:15:05 +0200

    DOM and JQUERY

--- DOM and JQUERY/app.js
+++ DOM and JQUERY/app.js
@@ -1,3 +1,15 @@
 let headingElement = document.getElementById('main-heading');
 let newHeadingText = prompt('Please provide a new heading:');
+// Змінюємо текст заголовка використовуючи JQUERY
 $('#main-heading').text(newHeadingText);
+
+// Створюємо нові елементи за допомогою JQUERY
+
+for (let i = 0; i < 3; i++) {
+    let hobby = prompt('Tell me one of your hobbies!');
+    $('body').append('<p>' + hobby + '<p>');
+}
+
+// Анімуємо елементи за допомогою JQUERY
+
+$('h1').fadeOut(3000).fadeIn(2000);
