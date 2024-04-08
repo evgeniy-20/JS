f2178b579f7e0f70cbe5814a7646bf742dc1d02b
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-04-08 23:00:27 +0300

    Інтерактивне програмування

--- Інтерактивне програмування/app.js
+++ Інтерактивне програмування/app.js
@@ -1,1 +1,25 @@
 // Затримуємо код за допомогою settimeout
+
+let timeUp = function () {
+    alert('Time`s up!');
+}
+
+setTimeout(timeUp, 5000);
+
+// Скасовуємо паузу
+
+let doHomeworkAratm = function () {
+    alert('Hey! You need to do your homework!');
+};
+
+let timeoutId = setTimeout(doHomeworkAratm, 60000);
+clearTimeout(timeoutId);
+
+// Викликаємо код декілька разв за допомогою setinerval
+
+let counter = 1;
+
+let printMessege = function () {
+    console.log('You have been staring at your console for ' + counter + ' seconds');
+    counter++;
+}
