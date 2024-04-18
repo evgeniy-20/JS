466430391ed86b5382d988c12d92ca5f7556bf30
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-04-18 08:18:00 +0300

    Multiple objects of the same class in OOP in JavaScript

---
+++ Підручник ООП JavaScript/Multiple objects of the same class in OOP in JavaScript/1.js
@@ -0,0 +1,10 @@
+// Створіть кілька об'єктів класу Employee.
+
+class Employee {
+
+}
+
+let employee1 = new Employee;
+let employee2 = new Employee;
+
+console.log(employee1);
---
+++ Підручник ООП JavaScript/Multiple objects of the same class in OOP in JavaScript/2.js
@@ -0,0 +1,14 @@
+// Запишіть у властивість кожного працівника його ім'я та зарплату.
+
+class Employee {
+
+}
+
+let employee1 = new Employee;
+let employee2 = new Employee;
+
+employee1.name = 'Yevhen';
+employee2.sallary = 3000;
+
+console.log(employee1);
+console.log(employee2);
