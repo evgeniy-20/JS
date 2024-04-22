e7d1c9aa442be3a02afc7fcc86f165d89da09e37
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-04-22 23:13:34 +0300

    Element canvas

---
+++ Element canvas/canvas.js
@@ -0,0 +1,24 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+
+// Head
+
+ctx.fillRect(76, 50, 50, 50);
+
+// Neck
+
+ctx.fillRect(76, 100, 10, 30);
+
+// arms
+
+ctx.fillRect(10, 150, 300, 20);
+
+// body
+
+ctx.fillRect(50, 150, 100, 100);
+
+
+// Ноги
+
+ctx.fillRect(70, 150, 20, 50);
+ctx.fillRect(110, 150, 20, 50);
