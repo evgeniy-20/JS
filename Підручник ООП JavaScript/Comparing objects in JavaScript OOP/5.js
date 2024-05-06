5017269c0ec477fed00b5871e9b048c71540823f
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-05-06 11:44:54 +0300

    Comparing objects in JavaScript OOP

---
+++ Підручник ООП JavaScript/Comparing objects in JavaScript OOP/5.js
@@ -0,0 +1,12 @@
+// Розкажіть, яким буде результат порівняння у наступному коді:
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let emp1 = new Employee('john');
+let emp2 = emp1;
+
+console.log(emp1 === emp2);
---
+++ Підручник ООП JavaScript/Comparing objects in JavaScript OOP/6.js
@@ -0,0 +1,12 @@
+// Розкажіть, яким буде результат порівняння у наступному коді:
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let emp1 = new Employee('john');
+let emp2 = new Employee('eric');
+
+console.log(emp1 !== emp2);
