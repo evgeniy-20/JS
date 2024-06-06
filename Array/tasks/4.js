8a1c9a5edd2ab9f19d0396b0ab958b1f5f980064
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-06-06 23:52:57 +0300

    task-4

---
+++ Array/tasks/4.js
@@ -0,0 +1,7 @@
+// Сортування: Напишіть функцію, яка приймає масив чисел і повертає відсортований масив.
+
+const getNumbers = (numbers) => {
+    return numbers.sort();
+}
+
+console.log(getNumbers([2, 3, 5, 4, 9, 6]));
