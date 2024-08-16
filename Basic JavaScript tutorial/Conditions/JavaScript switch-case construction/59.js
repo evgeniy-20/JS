a4204acb739d56355c457bfabfcffad4171273cc
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-08-16 21:43:09 +0300

    conditions

---
+++ Basic JavaScript tutorial/Conditions/JavaScript switch-case construction/59.js
@@ -0,0 +1,18 @@
+// Перепишіть наступний код через switch-case:
+
+let lang = 'ua';
+
+switch (lang) {
+    case 'ua':
+        console.log('ukranian');
+        break;
+    case 'en':
+        console.log('english');
+        break;
+    case 'de':
+        console.log('germany');
+        break;
+    default:
+        console.log('мова не знайдена!!!');
+        break;
+}
