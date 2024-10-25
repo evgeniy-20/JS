2a692b6fc1c57e578c1bb8caa549f551bee7a030
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-10-25 06:28:24 +0300

    Editing a single element in JavaScript

--- Basic JavaScript tutorial/Practice/Editing a single element in JavaScript/1.js
+++ Basic JavaScript tutorial/Practice/Editing a single element in JavaScript/1.js
@@ -1,9 +1,8 @@
-// Модифікуйте наведений вище код так, щоб текст абзацу змінювався не через втрату фокусу, а при введенні тексту в інпут.
+// Модифікуйте наведений вище код так, щоб текст абзацу змінювався не за втратою фокусу, а в міру введення тексту в інпут.
+
 let elem = document.querySelector('#elem');
 let input = document.querySelector('#input');
-
 input.value = elem.textContent;
-
 input.addEventListener('input', function () {
     elem.textContent = this.value;
-});
+})
