9df950de0c34dfd15fa10e7eca3ef581ba4bef1c
evgeniy-20 <evgeniy.prokopiv@gmail.com>
2024-11-05 05:42:26 +0200

    Function for creating HTML tables in JavaScript

--- Basic JavaScript tutorial/Functions and DOM/Passing a group of DOM elements by function parameters/3.js
+++ Basic JavaScript tutorial/Functions and DOM/Passing a group of DOM elements by function parameters/3.js
@@ -1,1 +1,15 @@
 // Дано масив і ul. За допомогою функції appendElem, створеної в попередньому завданні, запишіть кожен елемент масиву в окрему li в цьому ul.
+function appendElem(ulElement, text) {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ulElement.appendChild(li);
+}
+
+// Основний код
+const ulElement = document.querySelector('ul'); // Посилання на ul
+const items = ["Елемент 1", "Елемент 2", "Елемент 3"]; // Масив елементів
+
+// Додаємо кожен елемент масиву в ul як li
+for (let item of items) {
+    appendElem(ulElement, item);
+}
